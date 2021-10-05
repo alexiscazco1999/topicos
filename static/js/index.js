be6b1dec2242b3c7527e01fa85e868766d285567
alexiscazco1999 <89862680+alexiscazco1999@users.noreply.github.com>
2021-10-05 12:53:12 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,29 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function LED1_On() {
-	//alert("led on");
-	console.log("led on");
-	//document.getElementById("sensor").innerHTML="led on";
-	message = new Paho.MQTT.Message("ON");
-    	message.destinationName = "alexiscazco31@gmail.com/t1";
-    	client.send(message);
-  
+function MostrarSensor1() {
+
+  var Sensor1=document.getElementById("sensor1");//Muestra el primer valor en la etiqueta
+  var HiddenSensor1=Sensor1.getAttribute("hidden");
+
+  if(HiddenSensor1){
+    Sensor1.removeAttribute("hidden");
+  }else{
+    Sensor1.setAttribute("hidden", "hidden");
+  }
+
 }
-function LED1_Off(){	
-	//alert("led off");
-	console.log("led off");
-	message = new Paho.MQTT.Message("OFF");
-    	message.destinationName = "alexiscazco31@gmail.com/t1";
-    	client.send(message);
-	//document.getElementById("sensor").innerHTML="led off";
+
+function MostrarSensor2(){  
+
+  var Sensor2=document.getElementById("sensor2");//Muestra el primer valor en la etiqueta
+  var HiddenSensor2=Sensor2.getAttribute("hidden");
+
+  if(HiddenSensor2){
+    Sensor2.removeAttribute("hidden");
+  }else{
+    Sensor2.setAttribute("hidden", "hidden");
+  }
+  
 }
 
 
@@ -64,9 +72,13 @@ function LED1_Off(){
 
   // called when a message arrives
   function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-    var a = message.payloadString.split("+")
-	  document.getElementById("sensor1").innerHTML=a[0];
-    document.getElementById("sensor2").innerHTML=a[1];  
+    console.log("onMessageArrived:"+message.payloadString);//Se muestra en la consola el mensaje recibido
+    
+    var Mensaje=message.payloadString;//Se guarda el mensaje en una variable
+
+    var Sensores=Mensaje.split('_');//Divide el formato en que llegan los valores a razón del espacio en blanco
+
+    document.getElementById("sensor1").innerHTML=Sensores[0]+"- Estado: "+Sensores[1];//Muestra el primer valor en la etiqueta
+    document.getElementById("sensor2").innerHTML=Sensores[0]+"- Estado: "+Sensores[2];//Muestra el segundo valor en la etiqueta 
   }
   
